3e16034a1f8c336b15af4319c3240ed7fd818c60
Carlos Correa <33291776+Deadflight@users.noreply.github.com>
2021-08-07 17:27:24 -0400

    Meta Tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,18 @@ import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
+import Head from 'next/head'
 
 const Home = () => {
   return (
+    <>
+    <Head>
+      <title>Carlos Correa</title>  
+      <meta property='og:title' content='Carlos Correa'/>
+      <meta property='og:image' content='./public/images/ScreenShot.PNG'/>
+      <meta property='og:description' content='Carlos Correa Portfolio'/>
+      <meta property='og:url' content='//https://carloscorreaportfolio.netlify.app/'/>
+    </Head>
     <Layout>
       <Section grid>
         <Hero />
@@ -19,7 +28,9 @@ const Home = () => {
       <Timeline />
       {/*<Acomplishments />*/}
     </Layout>
+    </>
   );
+  
 };
 
 export default Home;
