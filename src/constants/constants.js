41b35375e4827df9cb4f4a30e93abb54c40b13af
Carlos <correamillancarlos@gmail.com>
2022-09-13 14:06:30 -0400

    [Update] Cheeky Bonsai and Teslo Shop projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,73 +1,90 @@
 export const projects = [
-  {
-    title: 'CryptoVerse',
-    description: "Crypto App using React, coinranking API, Bing News Search API, Redux Toolkit",
-      image: '/images/cryptoverse.PNG',
-      tags: ['React', 'API Rest', 'Redux Toolkit'],
-    source: 'https://github.com/Deadflight/cryptoverse-react-app',
-    visit: 'https://cryptoversedeadflight.netlify.app/',
-    id: 0,
-  },
-  {
-    title: 'Travel Companion',
-    description: "Travel Companion, an React app, working with Travel Advisor API, Google Maps and Open Weather Map API",
-    image : '/images/travelcompanion.PNG',
-    tags: ['React, API REST, Google Maps, Material-UI'],
-    source: 'https://github.com/Deadflight/travel-companion-react',
-    visit: 'https://travelcompaniondeadflight.netlify.app',
-    id: 1
-  },
-  {
-    title: 'Pokedex',
-    description: "Pokedex using Next.js, i18n,React Query, Styled Components, PokeAPI, SSR, ISR, SSG, Material UI",
-      image: '/images/pokedex.PNG',
-      tags: ['Next.js','API REST', 'Material UI', 'React Query', 'i18n'],
-    source: 'https://github.com/Deadflight/pokedex-nextjs',
-    visit: 'https://pokedex-nextjs-beta.vercel.app/',
-    id: 2,
-  },
-  {
-    title: 'Open Jira',
-    description: "Open Jira, Jira similar app, working with NextJs, Material UI, TypeScript, Heroku, MongoDB, Mongoose",
-    image: '/images/open-jira.png',
-    tags: ['Next.Js, TypeScript, MongoDB, Material-UI, Mongoose, Context API'],
-    source: 'https://github.com/Deadflight/open-jira',
-    visit: 'https://open-jira-one.vercel.app',
-    id: 3
-  },
-  {
-    title: 'Plantpedia',
-    description:"With Platzi's Next.Js courses Plantpedia learning about Server Side Rendering (SSR), Static Side Generation (SSG), Incremental Static Regeneration (ISR)",
-    image: '/images/plantpedia.PNG',
-    tags: ['Next.js', 'TypeScript', 'Contentful', 'Preview Mode'],
-    source: 'https://github.com/Deadflight/Platzi-Plantpedia',
-    visit: 'https://platzi-plantpedia-amber.vercel.app/',
-    id: 4,
-  },
-  {
-    title: 'Platzi Swag',
-    description: "My first aproach using Gatsby.js. This app was built with Gatbsy.js GraphQL, Styled Components, Stripe",
-    image: '/images/platzi-swag.png',
-    tags: ['Gatsby.Js', 'Styled Components', 'GraphQL', 'Stripe'],
-    source: 'https://github.com/Deadflight/platzi-swag',
-    visit: 'https://platziswagmain.gatsbyjs.io',
-    id: 5,
-  },
+	{
+		title: "Cheeky Bonsai",
+		description:
+			"Cheeky Bonsai science-backed UTI products made for balanced urinary tract health",
+		image: "/images/cheekybonsai.PNG",
+		source: "https://cheekybonsai.com",
+		tags: ["Gatsby.Js", "Shopify", "Styled Components", "GraphQL"],
+		visit: "https://cheekybonsai.com",
+		id: 0,
+	},
+	{
+		title: "Teslo Shop",
+		description:
+			"Teslo Shop ECommerce website made with Next.Js, MongoDB, Next Auth,Materil UI",
+		image: "/images/teslo-shop.PNG",
+		source: "https://github.com/Deadflight/nextjs-teslo-shop",
+		tags: ["Next.Js", "MongoDB", "Next Auth", "Materil UI"],
+		visit: "https://teslo-shop-carlos.herokuapp.com",
+		id: 1,
+	},
+	{
+		title: "CryptoVerse",
+		description:
+			"Crypto App using React, coinranking API, Bing News Search API, Redux Toolkit",
+		image: "/images/cryptoverse.PNG",
+		tags: ["React", "API Rest", "Redux Toolkit"],
+		source: "https://github.com/Deadflight/cryptoverse-react-app",
+		visit: "https://cryptoversedeadflight.netlify.app/",
+		id: 2,
+	},
+	{
+		title: "Pokedex",
+		description:
+			"Pokedex using Next.js, i18n,React Query, Styled Components, PokeAPI, SSR, ISR, SSG, Material UI",
+		image: "/images/pokedex.PNG",
+		tags: ["Next.js", "API REST", "Material UI", "React Query", "i18n"],
+		source: "https://github.com/Deadflight/pokedex-nextjs",
+		visit: "https://pokedex-nextjs-beta.vercel.app/",
+		id: 3,
+	},
+	{
+		title: "Travel Companion",
+		description:
+			"Travel Companion, an React app, working with Travel Advisor API, Google Maps and Open Weather Map API",
+		image: "/images/travelcompanion.PNG",
+		tags: ["React, API REST, Google Maps, Material-UI"],
+		source: "https://github.com/Deadflight/travel-companion-react",
+		visit: "https://travelcompaniondeadflight.netlify.app",
+		id: 4,
+	},
+	{
+		title: "Open Jira",
+		description:
+			"Open Jira, Jira similar app, working with NextJs, Material UI, TypeScript, Heroku, MongoDB, Mongoose",
+		image: "/images/open-jira.png",
+		tags: ["Next.Js, TypeScript, MongoDB, Material-UI, Mongoose, Context API"],
+		source: "https://github.com/Deadflight/open-jira",
+		visit: "https://open-jira-one.vercel.app",
+		id: 5,
+	},
+	// {
+	// 	title: "Platzi Swag",
+	// 	description:
+	// 		"My first aproach using Gatsby.js. This app was built with Gatbsy.js GraphQL, Styled Components, Stripe",
+	// 	image: "/images/platzi-swag.png",
+	// 	tags: ["Gatsby.Js", "Styled Components", "GraphQL", "Stripe"],
+	// 	source: "https://github.com/Deadflight/platzi-swag",
+	// 	visit: "https://platziswagmain.gatsbyjs.io",
+	// 	id: 5,
+	// },
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+	{ year: 2017, text: "Started my journey" },
+	{ year: 2018, text: "Worked as a freelance developer" },
+	{ year: 2019, text: "Founded JavaScript Mastery" },
+	{ year: 2020, text: "Shared my projects with the world" },
+	{ year: 2021, text: "Started my own platform" },
 ];
 
 export const MetaTags = {
-  og: {
-    title: 'Carlos Correa',
-    image: 'https://github.com/Deadflight/portfolio-react/blob/main/public/images/ScreenShot.PNG',
-    description: 'Carlos Correa Portfolio',
-    url: 'https://carloscorreaportfolio.netlify.app/'
-  }
+	og: {
+		title: "Carlos Correa",
+		image:
+			"https://github.com/Deadflight/portfolio-react/blob/main/public/images/ScreenShot.PNG",
+		description: "Carlos Correa Portfolio",
+		url: "https://carloscorreaportfolio.netlify.app/",
+	},
 };
