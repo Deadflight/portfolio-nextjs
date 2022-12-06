2e44176777b6f28552efadf187852f58de7cd16c
Carlos Correa <correamillancarlos@gmail.com>
2022-12-06 11:48:12 -0400

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -16,7 +16,7 @@ export const projects = [
 		image: "/images/teslo-shop.PNG",
 		source: "https://github.com/Deadflight/nextjs-teslo-shop",
 		tags: ["Next.Js", "MongoDB", "Next Auth", "Material UI"],
-		visit: "https://teslo-shop-carlos.herokuapp.com",
+		visit: "https://nextjs-teslo-shop-production.up.railway.app",
 		id: 1,
 	},
 	{
